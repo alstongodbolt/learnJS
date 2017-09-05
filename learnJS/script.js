c28f1edc4c4a1eee7108093f3a2db4d640b90260
Alston Godbolt <godbolta@onid.oregonstate.edu>
2017-09-04 20:58:10 -0500

    Add files via upload

---
+++ learnJS/script.js
@@ -0,0 +1,47 @@
+/* console.log('Hello World')
+var name = 'John '
+console.log(name)
+var lastName = 'Smith'
+console.log(name + lastName)
+var age = 26
+var feelsLike = 10
+console.log(age + feelsLike)
+console.log('My age:', 2017 - 1984)
+var fullAge = true
+console.log(fullAge)
+
+var name = 'John'
+var age = 26
+console.log(name + age)
+
+var job, isMarried
+console.log(job)
+
+job = 'teacher'
+isMarried = false
+console.log(name + ' age: ' + age + ' works as a ' + job + ' marriage equals ' + isMarried + '.')
+
+job = 26
+console.log(job)
+
+// get data from the console
+var last = prompt('What is the last name? ')
+console.log(last)
+
+alert('Hi ' + last + '!')
+*/
+// operators
+var birthYear = 2017 - 33
+console.log(birthYear)
+
+birthYear = 2017 - 33 * 2
+
+console.log(birthYear)
+
+var ageJohn = 38
+var ageMark = 30
+console.log(ageMark)
+ageMark++
+ageJohn = (3 + 5) * 4 - 6
+console.log(ageJohn)
+console.log(ageMark)
